bd4a96bef48fbeb604af8496ea9c7f5bf90d6a0e
Accio <kyosuke.7615@gmail.com>
2018-05-23 13:11:16 +0000

    kadai-tasklist

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <title>Taklistapp</title>
+    </head>
+    
+    <body>
+        @include('commons.error_tasks')
+        
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+        {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
+  
+@endsection
