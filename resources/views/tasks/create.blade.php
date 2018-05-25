b3af61cf6df3bf163f5e0d74563372195ea9e1bf
Accio <kyosuke.7615@gmail.com>
2018-05-25 20:37:34 +0000

    add grid bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,10 @@
     <h1>タスク新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="col-md-8 col-sm-offset-2">
+                    <div class="col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
                 <div class="form-group">
@@ -21,6 +24,9 @@
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
             {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,7 +6,10 @@
     
 
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="col-md-8 col-sm-offset-2">
+                    <div class="col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
@@ -21,6 +24,9 @@
                 {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
             {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
