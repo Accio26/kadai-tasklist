7bda93d2b96f1126b4e02858736fa29673cf6eb9
Accio <kyosuke.7615@gmail.com>
2018-05-26 11:07:38 +0000

    div 1 bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,10 +5,7 @@
     <h1>タスク新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-12">
-            <div class="col-sm-8 col-sm-offset-2">
-                <div class="col-md-8 col-sm-offset-2">
-                    <div class="col-lg-6 col-lg-offset-3">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
                 <div class="form-group">
@@ -23,10 +20,8 @@
 
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
+
             {!! Form::close() !!}
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 
